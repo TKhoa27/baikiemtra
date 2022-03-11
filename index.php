23f91fb34a33849709339690860fac755872312e
TKhoa27010707 <tieukhoa271@gmail.com>
2022-03-11 15:28:58 +0700

    khoa

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
         <form>
             <h1>user name:</h1>
             <input type="text" name="txtUserName">
+            <h1>password:</h1>
+            <input type="password" name="txtMatKhau">
         </form>
     </body>
 </html>
